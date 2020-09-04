47b0f2b69fa3e3b056b74446f5bb53e8b56394c7
Jeiner Noriega <bigal2331@users.noreply.github.com>
2020-09-04 12:26:49 -0400

    updated App

---
+++ src/components/Translate.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import DropDown from "./Dropdown";
+import Convert from "./Convert";
+
+const options = [
+  { label: "Afrikanns", value: "af" },
+  { label: "Arabic", value: "ar" },
+  { label: "Hindi", value: "hi" }
+];
+
+const Translate = () => {
+  const [selectedLang, setSelectedLang] = useState(options[0]);
+  const [term, setTerm] = useState("");
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label className="label">Enter Text</label>
+          <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        </div>
+      </div>
+      <DropDown
+        label="Select a Language"
+        selected={selectedLang}
+        setSelected={setSelectedLang}
+        options={options}
+      />
+      <h3 className="ui header">Output</h3>
+      {term ? <Convert languange={selectedLang.value} text={term} /> : null}
+    </div>
+  );
+};
+
+export default Translate;
