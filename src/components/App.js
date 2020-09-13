411eb251a1627e2874b28219e3e4a2974317d404
Jeiner Noriega <bigal2331@users.noreply.github.com>
2020-09-13 19:58:52 -0400

    implemented navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,38 @@
 import React, { useState } from "react";
-// import SearchBar from "./SearchBar";
-// import DropDown from "./Dropdown";
+import SearchBar from "./SearchBar";
+import Accordion from "./Accordion";
+import DropDown from "./Dropdown";
+import Route from "./Route";
 import Translate from "./T";
+import Header from "./Header";
 
 const items = [
   { label: "Red", value: "red" },
   { label: "Blue", value: "blue" },
-  { label: "Green", value: "green" },
+  { label: "Green", value: "green" }
 ];
 const App = (props) => {
   const [selectedColor, setColor] = useState(items[0]);
-
   return (
     <div className="ui container">
-      {/* <SearchBar /> */}
-      {/* <DropDown
-        setColor={setColor}
-        selectedColor={selectedColor}
-        options={items}
-      /> */}
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <SearchBar />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+      <Route path="/dropdown">
+        <DropDown
+          label="Select a color"
+          setSelected={setColor}
+          selected={selectedColor}
+          options={items}
+        />
+      </Route>
     </div>
   );
 };
--- src/components/Convert.js
+++ src/components/Convert.js
@@ -4,23 +4,33 @@ const KEY = "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM";
 
 const Convert = ({ languange, text }) => {
   const [translation, setTranslation] = useState("");
+  const [debouncedText, setDebouncedText] = useState(translation);
+  useEffect(() => {
+    const TimeoutId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+
+    return () => {
+      clearTimeout(TimeoutId);
+    };
+  }, [text]);
 
   useEffect(() => {
     const translate = async () => {
-      const URL = "https://translation.googleapis.com/languange/translate/v2";
+      const URL = "https://translation.googleapis.com/language/translate/v2";
       const requestBody = {};
       const requestQueryStrings = {
         params: {
-          q: text,
+          q: debouncedText,
           target: languange.value,
           key: KEY
         }
       };
       const { data } = await axios.post(URL, requestBody, requestQueryStrings);
-      setTranslation("Hola");
+      setTranslation(data.data.translation[0].translatedText);
     };
     translate();
-  }, [languange, text]);
+  }, [languange, debouncedText]);
 
   return <div>{translation}</div>;
 };
